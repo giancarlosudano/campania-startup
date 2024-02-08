4c963bd929546831120e9ff3516666006ed19fa8
Giancarlo Sudano <gisudano@microsoft.com>
2024-02-08 05:26:58 +0100

    ft traiettorie

--- code/pages/1_Ricerca_Traiettorie.py
+++ code/pages/1_Ricerca_Traiettorie.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 import os
 import traceback
 from datetime import datetime
@@ -6,86 +7,92 @@ import pandas as pd
 from langchain_openai import AzureChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.callbacks import StreamlitCallbackHandler
 import xml.etree.ElementTree as ET
 from dotenv import load_dotenv
-import base64
 import datetime
-import glob
-import json
-import openai
 import os
-import requests
-import sys
 import re
-import lib.common as common
 import pandas as pd
 import streamlit as st
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, ColumnsAutoSizeMode
+import yaml
+from yaml.loader import SafeLoader
+
+
+def get_traiettorie():
+	try:
+		llm = AzureChatOpenAI(
+			azure_endpoint=os.getenv("AZURE_OPENAI_BASE"), 
+			api_key=os.getenv("AZURE_OPENAI_KEY"),
+			api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+			max_tokens=1000,
+			temperature=0,
+			deployment_name=os.getenv("AZURE_OPENAI_MODEL"),
+			model_name=os.getenv("AZURE_OPENAI_MODEL_NAME"),
+			streaming=False
+		)
+
+		with open(os.path.join('data', 'traiettorie.md'), 'r', encoding='utf-8') as file:
+			tabella = file.read()
+
+		prompt_composed = """Data la seguente descrizione di una attività delimitata da ###
+identifica dalla tabella fornita la lista delle righe che si avvicinano maggiormente alla descrizione fornita.
+
+attività:
+### 
+{input}
+###
+
+Tabella:
+{tabella}
+
+- Nella risposta fornisci sempre il contenuto di tutte le colonne della tabella per ciascuna riga identificata.
+
+Risposta:
+"""
+		prompt = ChatPromptTemplate.from_messages([
+			("system", "You are an AI assistant."),
+			("user", prompt_composed)
+		])
+
+		output_parser = StrOutputParser()
+		chain = prompt | llm | output_parser
+		response = chain.invoke({"input": st.session_state['descrizione'], "tabella": tabella})
+		st.write(response)
+
+	except Exception as e:
+		st.error(traceback.format_exc())
 
 try:
 	st.set_page_config(page_title="Regione Campania - Ricerca Traiettorie", page_icon=os.path.join('images','favicon.ico'), layout="wide", menu_items=None)
-	st.title("Scelta Range Date")
+	st.title("Ricerca traiettorie")
+	load_dotenv()
 	st.sidebar.image(os.path.join('images','logo.png'), use_column_width=True)
-	import streamlit_authenticator as stauth
 
-	import yaml
-	from yaml.loader import SafeLoader
+	st.write(os.getenv("AZURE_OPENAI_BASE"))
+	# with open('config.yaml') as file:
+	# 	config = yaml.load(file, Loader=SafeLoader)
 
-	with open('config.yaml') as file:
-		config = yaml.load(file, Loader=SafeLoader)
+	# authenticator = stauth.Authenticate(
+	# 	config['credentials'],
+	# 	config['cookie']['name'],
+	# 	config['cookie']['key'],
+	# 	config['cookie']['expiry_days'],
+	# 	config['preauthorized']
+	# )
 
-	authenticator = stauth.Authenticate(
-		config['credentials'],
-		config['cookie']['name'],
-		config['cookie']['key'],
-		config['cookie']['expiry_days'],
-		config['preauthorized']
-	)
+	# if st.session_state["authentication_status"]:
 
-	if st.session_state["authentication_status"]:		
-		load_dotenv()
-		st.image(os.path.join('images','Slide1.JPG'), use_column_width=True)
-
-		colonne = ['Data email', 'Oggetto', 'Da']
-		df = pd.DataFrame(columns=colonne)
-		i = 0
-		for root, dirs, files in os.walk(os.path.join('ldv')):
-			for name in dirs:
-				file_msg = os.path.join('ldv', name, 'msg_data.txt')
-				with open(file_msg, 'r') as file:
-					content = file.read()
-					from_pattern = r"from: (.+)"
-					from_match = re.search(from_pattern, content)
-					from_value = from_match.group(1) if from_match else None
-					subject_pattern = r"subject: (.+)"
-					subject_match = re.search(subject_pattern, content)
-					subject_value = subject_match.group(1) if subject_match else None
-					data_convertita = datetime.datetime.strptime(name, '%Y%m%d %H%M%S')
-					if data_inizio <= data_convertita.date() <= data_fine:
-						df.loc[i] = [name, subject_value, from_value]
-				i += 1
-
-		# select the columns you want the users to see
-		gb = GridOptionsBuilder.from_dataframe(df)
-		# configure selection
-		gb.configure_selection(selection_mode="single", use_checkbox=True)
-		gb.configure_side_bar()
-		gridOptions = gb.build()
-
-		data = AgGrid(df,
-					gridOptions=gridOptions,
-					enable_enterprise_modules=True,
-					allow_unsafe_jscode=True,
-					update_mode=GridUpdateMode.SELECTION_CHANGED,
-					columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS)
-
-		if st.button("Ricerca Traiettorie"):
-			st.toast("TODO")
-
-	elif st.session_state["authentication_status"] is False:
-		st.error('Username/password is incorrect')
-	elif st.session_state["authentication_status"] is None:
-		st.warning('Please enter your username and password')
+	st.text_area("Descrizione", key="descrizione", height=200)
+	container_button = st.container()
+	container_streaming = st.container()
+	container_response = st.container()
+	container_button.button("Ricerca Traiettorie...", key="button", on_click=get_traiettorie)
+	
+	# elif st.session_state["authentication_status"] is False:
+	# 	st.error('Username/password is incorrect')
+	# elif st.session_state["authentication_status"] is None:
+	# 	st.warning('Please enter your username and password')
 
 except Exception as e:
 	st.error(traceback.format_exc())
